0584b1f19144ab49cc9f3a34c7ee09cc680729b5
Dikshika <dikshu961@gmail.com>
2025-10-30 23:14:13 +0530

    added the frontend URL

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     allow_origins=["http://localhost:5173", 
                    "http://127.0.0.1:5173", 
                    "http://localhost:5174",
-                   "https://check-it.pages.dev/"
+                   "https://check-it.pages.dev/",
+                   "https://frontend-v2-awl.pages.dev/"
                    ],  # Vite default port
     allow_credentials=True,
     allow_methods=["*"],
